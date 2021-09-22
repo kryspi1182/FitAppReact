b5c8fedbdc8032a078016345a253883afc84afce
Krystian Wysocki <krystian1182@wp.pl>
2021-09-22 17:51:23 +0200

    Forgot to commit one interface :)

---
+++ FitAppReact.Interfaces.Infrastructure/UserService/IAppUserSrv.cs
@@ -0,0 +1,14 @@
+﻿using FitAppReact.Common.ModelsDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitAppReact.Interfaces.Infrastructure.UserService
+{
+    public interface IAppUserSrv
+    {
+        public AppUserDTO GetUserById(string id);
+    }
+}
