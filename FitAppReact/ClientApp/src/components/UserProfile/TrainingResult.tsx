c4a4494ebb1075c2b71fda88b370198189f87289
Krystian Wysocki <krystian1182@wp.pl>
2021-11-23 18:51:13 +0100

    Quick fix

--- FitAppReact/ClientApp/src/components/UserProfile/TrainingResult.tsx
+++ FitAppReact/ClientApp/src/components/UserProfile/TrainingResult.tsx
@@ -1,17 +1,7 @@
 import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { EntityId } from '@reduxjs/toolkit';
-
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import { Container, Row } from 'reactstrap';
-import { getAllEnumEntries, getAllEnumKeys, getAllEnumValues } from 'enum-for';
 
 import { selectAllUserTrainings } from '../../store/userTrainingsSlice';
-import { TrainingCategoryEnum } from '../../models/enums/TrainingCategoryEnum';
-import TrainingBox from '../common/TrainingBox';
-import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@material-ui/core';
 import TrainingList from '../common/TrainingList';
 
 const TrainingResult: React.FC = () => {
